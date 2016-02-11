4964e3ab0d0913ae4730f10614c89daa4c50a321
Yaroslav <yaroslav.mamrokha@gmail.com>
2016-02-11 21:51:11 +0200

    Task formatting.
    Added comments, added non integer input handling.

--- Task4.cpp
+++ Task4.cpp
@@ -1,27 +1,43 @@
 #include<iostream>
 #include<string>
 #include <vector>
-using std::cout;
-using std::cin;
-using std::cerr;
-using std::vector;
-using std::string;
+using namespace std;
+
+/*
+*@[brief]: Function check if istream object state is set to failbit, shows error and fix input stream;
+*@[in]: istream object needed for fail check;
+*@[out]: bool statement that shows function result, true if failbit enabled and false otherwise;
+*/
+
+bool isbadinput(istream& in) {
+	if (in.fail()) {
+		cout << "Bad input\n";
+		in.clear();
+		in.ignore();
+		return true;
+	}
+	return false;
+}
 
 void main() {
 	vector<string> Drinks{ "Cola","Pepsi","Fanta","Sprite","7up" };
-	{
-		int i = 0;
-		cout << "Choose beloved drink\n";
-		for (auto x : Drinks) {
-			cout << ++i << ") " << x << "\n";
-		}
-	}
 	int choice = 0;
-	cin >> choice;
+	bool check = true;
+	while (check) {
+		{
+			int i = 0;
+			cout << "Choose beloved drink\n";
+			for (auto x : Drinks) {
+				cout << ++i << ") " << x << "\n";
+			}
+		}
+		cin >> choice;
+		check = isbadinput(cin);
+	}
 		try {
 			cout << Drinks.at(choice - 1) << "\n";    //if subscript out of range at throws out_of_range exeption
 		}
-		catch (std::out_of_range) {
+		catch (out_of_range) {
 			cout << "Your choice is not vaild, take your money back\n";
 			return;
 		}
